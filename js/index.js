b9dcfcfa6020254e25a771967bee2f8484e9075b
peter-sq <awe.petersq@gmail.com>
2021-06-03 21:59:41 +0100

    updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', () => {
+    document.querySelector('.main').classList.remove('hidden');
+    document.querySelector('.home-section').classList.add('active');
+    // page Loader
+    document.querySelector('.page-loader').classList.add('fade-out')
+    setTimeout(() => {
+        document.querySelector('.page-loader').classList.style.display = 'none'
+
+    }, 600)
+
+})
+
 //Navbar Toggle
 const navToggler = document.querySelector('.nav-toggler');
 navToggler.addEventListener('click', () =>{
